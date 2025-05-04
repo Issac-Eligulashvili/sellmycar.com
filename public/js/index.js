9bea1e0696b8722a9342f4aad2fde4140b35fab0
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-05-04 14:08:12 -0400

    impelmented rerouting to different pages based on user status as dealership and implmented vue fetching with backend

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,27 @@
-$.ajax({
-     type: "GET",
-     url: "/user/data",
-     headers: {
-          'Content-Type': 'application/json',
-          
-     },
-     xhrFields: {
-          withCredentials: true,
-     },
-     success: function (response) {
-          console.log(response);
-     },
-     error: function (xhr, status, error) {
-          if(xhr.status === 401) {
-               window.location.href='/login.html'
+$(document).ready(function () {
+     $.ajax({
+          type: "GET",
+          url: "/user/data",
+          headers: {
+               'Content-Type': 'application/json',
+               
+          },
+          xhrFields: {
+               withCredentials: true,
+          },
+          success: function (response) {
+               const data = response.data;
+               if (data.isDealer) {
+                    window.location.href = "/dealer.html"
+               } else {
+                    window.location.href = "/user.html";
+               }
+          },
+          error: function (xhr, status, error) {
+               if(xhr.status === 401) {
+                    window.location.href='/login.html'
+               }
           }
-     }
+     });
 });
+
---
+++ public/js/userMain.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+     
+});
+
+const app = Vue.createApp({
+     data(){
+          return {
+               data: {},
+               isLoading: true,
+          }
+     },
+     methods: {
+          getData() {
+               $.ajax({
+                    type: "GET",
+                    url: "/user/data",
+                    headers: {
+                         'Content-Type': 'application/json',
+                         
+                    },
+                    xhrFields: {
+                         withCredentials: true,
+                    },
+                    success: (response) => {
+                         this.data = response.data;
+                         this.isLoading = false;
+                    },
+                    error: function (xhr, status, error) {
+                         if(xhr.status === 401) {
+                              window.location.href='/login.html'
+                         }
+                    }
+               });
+          },
+          formatState(state) {
+               const states = {
+                    "Alabama": "AL",
+                    "Alaska": "AK",
+                    "Arizona": "AZ",
+                    "Arkansas": "AR",
+                    "California": "CA",
+                    "Colorado": "CO",
+                    "Connecticut": "CT",
+                    "Delaware": "DE",
+                    "Florida": "FL",
+                    "Georgia": "GA",
+                    "Hawaii": "HI",
+                    "Idaho": "ID",
+                    "Illinois": "IL",
+                    "Indiana": "IN",
+                    "Iowa": "IA",
+                    "Kansas": "KS",
+                    "Kentucky": "KY",
+                    "Louisiana": "LA",
+                    "Maine": "ME",
+                    "Maryland": "MD",
+                    "Massachusetts": "MA",
+                    "Michigan": "MI",
+                    "Minnesota": "MN",
+                    "Mississippi": "MS",
+                    "Missouri": "MO",
+                    "Montana": "MT",
+                    "Nebraska": "NE",
+                    "Nevada": "NV",
+                    "New Hampshire": "NH",
+                    "New Jersey": "NJ",
+                    "New Mexico": "NM",
+                    "New York": "NY",
+                    "North Carolina": "NC",
+                    "North Dakota": "ND",
+                    "Ohio": "OH",
+                    "Oklahoma": "OK",
+                    "Oregon": "OR",
+                    "Pennsylvania": "PA",
+                    "Rhode Island": "RI",
+                    "South Carolina": "SC",
+                    "South Dakota": "SD",
+                    "Tennessee": "TN",
+                    "Texas": "TX",
+                    "Utah": "UT",
+                    "Vermont": "VT",
+                    "Virginia": "VA",
+                    "Washington": "WA",
+                    "West Virginia": "WV",
+                    "Wisconsin": "WI",
+                    "Wyoming": "WY"
+               };
+               return states[state];
+          }
+     },
+     mounted() {
+          this.getData();
+     },
+     watch: {
+          data(newData) {
+               console.log(newData);
+          }
+     }     
+}).mount("#app");
